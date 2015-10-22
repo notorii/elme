f88449a3eff5fe91d49d5d70cd8ec94c1d2f0c39
mbignell <maggie.bignell@gmail.com>
2015-10-21 21:27:22 -0700

    initial commit

---
+++ Codepath_CBT/Item1ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  Item1ViewController.swift
+//  Codepath_CBT
+//
+//  Created by Margaret Bignell on 10/21/15.
+//  Copyright © 2015 maggled. All rights reserved.
+//
+
+import UIKit
+
+class Item1ViewController: UIViewController {
+
+    @IBOutlet weak var closeButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClosePress(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
