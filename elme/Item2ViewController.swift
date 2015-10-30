e179960483aff49380927094f0d3825cad4e9214
mbignell <maggie.bignell@gmail.com>
2015-10-29 21:08:33 -0700

    step details stupid ghost thing 👻

--- elme/Item2ViewController.swift
+++ elme/Item2ViewController.swift
@@ -11,9 +11,29 @@ import UIKit
 class Item2ViewController: UIViewController {
 
     @IBOutlet weak var closeButton: UIButton!
+    
+  //  @IBOutlet weak var stepTitle: UITextView!
+    
+    @IBOutlet weak var howDistressingLabel: UILabel!
+    @IBOutlet weak var distressLevelLabel: UILabel!
+    
+    @IBOutlet weak var remindMeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+
+    @IBOutlet weak var creationNoteLabel: UILabel!
+    @IBOutlet weak var thoughtsTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        closeButton.tintColor = mediumTextColor
+//        stepTitle.textColor = darkTextColor
+//        howDistressingLabel.textColor = mediumTextColor
+//        distressLevelLabel.textColor = mediumSecondaryTextColor
+//        remindMeLabel.textColor = mediumTextColor
+//        creationNoteLabel.textColor = mediumTextColor
+//        timeLabel.textColor = darkTextColor
+//        thoughtsTextView.textColor = darkTextColor
+//        
         // Do any additional setup after loading the view.
     }
 
@@ -22,9 +42,9 @@ class Item2ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onClosePress(sender: UIButton) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
+//    @IBAction func onClosePress(sender: UIButton) {
+//        dismissViewControllerAnimated(true, completion: nil)
+//    }
 
     /*
     // MARK: - Navigation
