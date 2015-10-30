13c5079cf9a38e73724566065181fb9ff005861f
mbignell <maggie.bignell@gmail.com>
2015-10-30 09:45:30 -0700

    borders, colors, step details oh my 👀✊🏻👾

--- elme/Item2ViewController.swift
+++ elme/Item2ViewController.swift
@@ -13,6 +13,7 @@ class Item2ViewController: UIViewController {
     @IBOutlet weak var closeButton: UIButton!
     
   //  @IBOutlet weak var stepTitle: UITextView!
+    @IBOutlet weak var stepDetailsView: UIView!
     
     @IBOutlet weak var howDistressingLabel: UILabel!
     @IBOutlet weak var distressLevelLabel: UILabel!
@@ -20,20 +21,33 @@ class Item2ViewController: UIViewController {
     @IBOutlet weak var remindMeLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
 
+    @IBOutlet weak var border1: UIView!
+    @IBOutlet weak var border2: UIView!
+    @IBOutlet weak var border3: UIView!
+    @IBOutlet weak var border4: UIView!
     @IBOutlet weak var creationNoteLabel: UILabel!
     @IBOutlet weak var thoughtsTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        closeButton.tintColor = mediumTextColor
-//        stepTitle.textColor = darkTextColor
-//        howDistressingLabel.textColor = mediumTextColor
-//        distressLevelLabel.textColor = mediumSecondaryTextColor
-//        remindMeLabel.textColor = mediumTextColor
-//        creationNoteLabel.textColor = mediumTextColor
-//        timeLabel.textColor = darkTextColor
-//        thoughtsTextView.textColor = darkTextColor
-//        
+        stepDetailsView.backgroundColor = lightBackgroundColor
+        stepDetailsView.layer.borderWidth = 1.0
+        stepDetailsView.layer.borderColor = borderColor.CGColor
+        closeButton.tintColor = mediumTextColor
+        howDistressingLabel.textColor = mediumTextColor
+        distressLevelLabel.textColor = mediumSecondaryTextColor
+        remindMeLabel.textColor = mediumTextColor
+        creationNoteLabel.textColor = mediumTextColor
+        timeLabel.textColor = darkTextColor
+        thoughtsTextView.textColor = darkTextColor
+        titleTextField.textColor = darkTextColor
+        
+        border1.backgroundColor = borderColor
+        border2.backgroundColor = borderColor
+        border3.backgroundColor = borderColor
+        border4.backgroundColor = borderColor
+        
         // Do any additional setup after loading the view.
     }
 
