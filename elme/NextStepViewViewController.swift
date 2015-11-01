418deb95abd66b9a36ef139e0547c87ccc1d6945
heat525 <prime.heather@gmail.com>
2015-10-31 17:04:40 -0700

    user can view next step
    sans giant check animation

---
+++ elme/NextStepViewViewController.swift
@@ -0,0 +1,46 @@
+//
+//  NextStepViewViewController.swift
+//  elme
+//
+//  Created by Prime, Heather(AWF) on 10/31/15.
+//  Copyright © 2015 maggled. All rights reserved.
+//
+
+import UIKit
+
+class NextStepViewViewController: UIViewController {
+
+    @IBOutlet weak var timeDateLabel: UILabel!
+    @IBOutlet weak var stepLabel: UILabel!
+    @IBOutlet weak var navLabel: UILabel!
+    @IBOutlet weak var navBarView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBOutlet weak var onCheckmarkTap: UIButton!
+    @IBAction func onMenuTap(sender: AnyObject) {
+    }
+    @IBAction func onRightChevronTap(sender: AnyObject) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
